49b8920a2a1b5d1b7ccabe847048797c6831887e
calderaevelyn016 <23308060610294@cetis61.edu.mx>
2026-05-05 08:08:44 -0500

    ....

--- src/main.py
+++ src/main.py
@@ -1,33 +1,46 @@
 import flet as ft
-from controllers.UsuariosController import AuthController
+from controllers.UserController import AuthController 
 from controllers.TareaController import TareaController
 from views.LoginView import LoginView
-from views.DashboardView import DashboardView
+from views.dashboardView import dashboardView  
 
 def start(page: ft.Page):
+    page.title="Sistema SIGE"
+    page.window_width=450
+    page.window_height=700
+    
     auth_ctrl = AuthController()
     task_ctrl = TareaController()
 
     def route_change(e):
         page.views.clear()
-        
+
         if page.route == "/":
-            page.add(ft.Text("Caso 1"))
             page.views.append(LoginView(page, auth_ctrl))
+            
         elif page.route == "/dashboard":
-            page.views.append(DashboardView(page, task_ctrl))
-        if not page.views:
-            page.views.append(
-                ft.View("/", [ft.Text("Error: Ruta no encontrada o vista vacía")])
-            )
+            page.views.append(dashboardView(page, task_ctrl))
             
         page.update()
+        
+    def view_pop(e):
+            if len(page.views)> 1:
+                page.views.pop()
+                top_view=page.views[-1]
+                page.go(top_view.route)
 
     page.on_route_change = route_change
-    page.go("/")
+    page.on_view_pop=view_pop
+    
+    if page.route == "/":
+        route_change(None)
+    else:
+        page.go("/")
+    
 
 def main():
     ft.app(target=start)
 
 if __name__ == "__main__":
     main()
+    #usuario,registro
